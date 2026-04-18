c9b408e367662541913075ab6a5145910bb6b84c
Shafeek-coder7594 <shafeek6648@gmail.com>
2026-04-18 22:42:58 +0530

    Added some fix

--- frontend/src/pages/products/ProductsPage.jsx
+++ frontend/src/pages/products/ProductsPage.jsx
@@ -69,7 +69,7 @@ const ProductsPage = () => {
           </div>
 
           {/* Sort */}
-          <div>
+          <div className="width-full">
             <h3 className="text-sm font-semibold text-gray-800 mb-2">Sort by:</h3>
 
             <select
